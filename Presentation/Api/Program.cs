04f8497b4cad38bc6731e709f3eb6bf551a62651
SerdarCogen <serdarcogen@gmail.com>
2025-06-08 16:13:46 +0300

    Initial Start

---
+++ Presentation/Api/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Youtube API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Youtube API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseAuthorization();
+app.MapControllers();
+
+await app.RunAsync();
